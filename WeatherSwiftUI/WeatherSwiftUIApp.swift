94e771ae7678ab0762b7deea854d1eaf43c0205d
ismehimeh <ismehimeh@gmail.com>
2024-05-13 16:04:24 +0300

    Initial commit

---
+++ WeatherSwiftUI/WeatherSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherSwiftUIApp.swift
+//  WeatherSwiftUI
+//
+//  Created by Sergei Vasilenko on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
